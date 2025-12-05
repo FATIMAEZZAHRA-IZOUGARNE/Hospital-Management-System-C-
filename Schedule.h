a50d3eab8a61dfc5167c113d6bf35c55fdad67b2
FATIMAEZZAHRA-IZOUGARNE <fatimaezzahraizougarne@gmail.com>
2025-12-06 00:58:05 +0100

    I removes some unnecessary functions
    Removed unused methods and simplified the Schedule class.

--- Schedule.h
+++ Schedule.h
@@ -8,7 +8,7 @@ namespace Hospital {
     class Schedule {
     private:
         vector<string> slots;
-        vector<bool> availability;     // true = libre, false = réservé
+        vector<bool> availability;     // true = libre, false = rÃ©servÃ©
         int count;
         int capacity;
 
@@ -19,19 +19,9 @@ namespace Hospital {
         bool bookSlot(const string& dt);
         void display() const;
 
-        bool isAvailable(int index) const {
-            if (index < 0 || index >= slots.size()) return false;
-            return availability[index];
-        }
-
-        string getSlot(int index) const {
-            if (index >= 0 && index < slots.size())
-                return slots[index];
-            return "";
-        }
         //--------- getters-----------
         const vector<string>& getSlots() const { return slots; }
-        const vector<bool>& getAvailability() const { return availability; }
     };
 
 }
+
